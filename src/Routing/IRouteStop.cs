512089135a6827029e54ef63433ba99234751d93
Austin Vandersluis <austin@vandersluis.club>
2026-01-28 16:12:04 -0800

    feat: Add Routing domain models for trips and stops

---
+++ src/Routing/IRouteStop.cs
@@ -0,0 +1,22 @@
+﻿using OutsourceTracker.Services;
+
+namespace OutsourceTracker.Routing;
+
+public interface IRouteStop<TID> : IModel<TID>
+{
+    string Name { get; set; }
+
+    string LocationCode { get; set; }
+
+    string Address { get; set; }
+
+    int Sequence { get; set; }
+
+    TID TripId { get; set; }
+
+    DateTimeOffset? ArrivalTime { get; set; }
+
+    DateTimeOffset? DepartureTime { get; set; }
+
+    decimal? OdometerAtArrival { get; set; }
+}
---
+++ src/Routing/RouteStop.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OutsourceTracker.Routing;
+
+public class RouteStop : IRouteStop<Guid>
+{
+    public Guid Id { get; set; } = Guid.CreateVersion7();
+
+    public Guid TripId { get; set; }
+
+    public string Name { get; set; }
+
+    public string LocationCode { get; set; }
+
+    public string Address { get; set; }
+
+    public int Sequence { get; set; }
+
+    public DateTimeOffset? ArrivalTime { get; set; }
+
+    public DateTimeOffset? DepartureTime { get; set; }
+
+    public decimal? OdometerAtArrival { get; set; }
+    
+}
