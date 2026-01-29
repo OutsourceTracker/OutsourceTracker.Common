cf281af76e29b5d37330884e81cca89a6f49a6d4
Austin Vandersluis <austin@vandersluis.club>
2026-01-28 16:12:04 -0800

    feat: Add Location domain models

---
+++ src/Locations/GeoLocation.cs
@@ -0,0 +1,13 @@
+﻿namespace OutsourceTracker.Locations;
+
+public class GeoLocation : IGeoLocation<Guid>
+{
+    public Guid Id { get; set; } = Guid.CreateVersion7();
+    public string ShortCode { get; set; }
+    public string FullName { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string ZipCode { get; set; }
+    public string FullAddress { get; set; }
+}
---
+++ src/Locations/IGeoLocation.cs
@@ -0,0 +1,18 @@
+﻿using OutsourceTracker.Services;
+
+namespace OutsourceTracker.Locations;
+
+public interface IGeoLocation<GID> : IModel<GID>
+{
+    string ShortCode { get; set; }
+
+    string FullName { get; set; }
+
+    string Address { get; set; }
+
+    string City { get; set; }
+
+    string State { get; set; }
+
+    string ZipCode { get; set; }
+}
