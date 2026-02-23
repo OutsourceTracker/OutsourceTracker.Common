56a2e2c0d1935aa6f7a3db3a8a364ac90b4a6ee2
Austin Vandersluis <austin@vandersluis.club>
2026-02-22 23:20:06 -0800

    Introduces role-based authorization
    Adds a role-based authorization system with predefined application roles and permissions.
    This allows for finer-grained control over access to different features.
    It also provides extension methods for easily checking roles on claims principals.

---
+++ src/Authorization/ApplicationRole.cs
@@ -0,0 +1,53 @@
+﻿using System.Reflection;
+using System.Security.Claims;
+
+namespace OutsourceTracker.Authorization;
+
+public readonly struct ApplicationRole
+{
+    public string Name { get; }
+
+    public string Permission { get; }
+
+    public ApplicationRole(string name, string permission)
+    {
+        Name = name;
+        Permission = permission;
+    }
+
+    public static ApplicationRole TrailerViewer { get; } = new ApplicationRole("Trailer Viewer", "Trailers.Read");
+    public static ApplicationRole TrailerAdmin { get; } = new ApplicationRole("Trailer Admin", "Trailers.Write");
+    public static ApplicationRole TrailerSpotter { get; } = new ApplicationRole("Trailer Spotter", "Trailers.UpdateLocation");
+    public static ApplicationRole ZoneViewer { get; } = new ApplicationRole("Zone Viewer", "Zones.Read");
+    public static ApplicationRole ZoneAdmin { get; } = new ApplicationRole("Zone Admin", "Zones.Write");
+
+    private static IReadOnlyDictionary<string, ApplicationRole> RoleCollection { get; }
+
+    static ApplicationRole()
+    {
+        var properties = typeof(ApplicationRole)
+            .GetProperties(BindingFlags.Public | BindingFlags.Static)
+            .Where(p => p.PropertyType == typeof(ApplicationRole));
+
+        Dictionary<string, ApplicationRole> roles = new Dictionary<string, ApplicationRole>();
+
+        foreach (var item in properties)
+        {
+            ApplicationRole role = (ApplicationRole)item.GetValue(null)!;
+            roles.Add(role.Permission, role);
+        }
+
+        RoleCollection = roles.AsReadOnly();
+    }
+
+    public static IEnumerable<ApplicationRole> GetUserRoles(ClaimsPrincipal claimsPrincipal)
+    {
+        foreach (var role in RoleCollection)
+        {
+            if (claimsPrincipal.IsInRole(role.Key))
+            {
+                yield return role.Value;
+            }
+        }
+    }
+}
---
+++ src/Authorization/AuthorizationExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace OutsourceTracker.Authorization;
+
+public static class AuthorizationExtensions
+{
+    public static bool IsInRole(this ClaimsPrincipal claim, ApplicationRole role) => claim.IsInRole(role.Permission);
+
+    public static IEnumerable<ApplicationRole> GetUserRoles(this ClaimsPrincipal claim) => ApplicationRole.GetUserRoles(claim);
+}
