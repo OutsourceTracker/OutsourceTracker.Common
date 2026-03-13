f3c3ccb9606928e844b15eb9fd24d9324827b747
Austin Vandersluis <austin@vandersluis.club>
2026-03-13 01:13:29 -0700

    Add: Introduce ITask interface and TaskType enum for new task system

---
+++ src/Tasks/ITask.cs
@@ -0,0 +1,14 @@
+﻿using OutsourceTracker.Services.ModelService;
+
+namespace OutsourceTracker.Tasks;
+
+public interface ITask<TID> : IServiceModel<TID> where TID : struct
+{
+    string AssignedTo { get; }
+
+    TaskType Type { get; }
+
+    string JsonData { get; }
+
+    DateTimeOffset? CompletedOn { get; }
+}
---
+++ src/Tasks/TaskType.cs
@@ -0,0 +1,14 @@
+﻿namespace OutsourceTracker.Tasks;
+
+/// <summary>
+/// Specifies the types of tasks that can be performed within the system.
+/// </summary>
+/// <remarks>Use this enumeration to categorize and identify different task operations. Each value represents a
+/// distinct task type that may be used for task management, filtering, or processing logic.</remarks>
+public enum TaskType : byte
+{
+    /// <summary>
+    /// Represents a trailer movement operation where when it's completed the trailer location is updated.
+    /// </summary>
+    TrailerMove = 0
+}
