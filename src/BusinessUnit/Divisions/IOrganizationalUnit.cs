b77d1e3a0efb18e8dc06d04f05cf04a907f8a978
Austin Vandersluis <austin@vandersluis.club>
2026-05-03 20:14:59 -0700

    Add: Business Unit Organizational Units

---
+++ src/BusinessUnit/Divisions/IOrganizationalUnit.cs
@@ -0,0 +1,14 @@
+﻿using OutsourceTracker.Services.ModelService;
+
+namespace OutsourceTracker.BusinessUnit.Divisions;
+
+public interface IOrganizationalUnit<TID> : IServiceModel<TID> where TID : struct
+{
+    string ShortCode { get; set; }
+
+    string Name { get; set; }
+
+    string Description { get; set; }
+
+    int TotalAccounts { get; set; }
+}
---
+++ src/BusinessUnit/Divisions/OrganizationalUnit.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace OutsourceTracker.BusinessUnit.Divisions;
+
+[Index(nameof(ShortCode), IsUnique = true, Name = "IX_OrganizationalUnit_ShortCode_Unique")]
+[Index(nameof(Name), IsUnique = false, Name = "IX_OrganizationalUnit_Name")]
+public class OrganizationalUnit : IOrganizationalUnit<Guid>
+{
+    [Key]
+    public Guid Id { get; set; } = default!;
+
+    public string ShortCode { get; set; } = default!;
+
+    public string Name { get; set; } = default!;
+
+    public string Description { get; set;  } = default!;
+
+    public int TotalAccounts { get; set; } = 0;
+
+    public DateTimeOffset CreatedOn { get; set; } = default!;
+
+    public bool Equals(Guid other) => Id.Equals(other);
+}
