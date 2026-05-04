fee0c58273a7c79e361f78ba2ad271009b05b98f
Austin Vandersluis <austin@vandersluis.club>
2026-05-03 20:14:59 -0700

    Add: Business Unit Organizational Accounts

---
+++ src/BusinessUnit/Accounts/IOrganizationAccount.cs
@@ -0,0 +1,12 @@
+﻿using OutsourceTracker.Services.ModelService;
+
+namespace OutsourceTracker.BusinessUnit.Accounts;
+
+public interface IOrganizationAccount<TID> : IServiceModel<TID> where TID : struct
+{
+    string ShortCode { get; set; }
+
+    string Name { get; set; }
+
+    TID OUID { get; set; }
+}
---
+++ src/BusinessUnit/Accounts/OrganizationalAccount.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace OutsourceTracker.BusinessUnit.Accounts;
+
+[Index(nameof(ShortCode), IsUnique = true, Name = "IX_OrganizationalAccount_ShortCode_Unique")]
+[Index(nameof(Name), IsUnique = false, Name = "IX_OrganizationalAccount_Name")]
+public class OrganizationalAccount : IOrganizationAccount<Guid>
+{
+    [Key]
+    public Guid Id { get; set; } = default;
+
+    public string ShortCode { get; set; } = default!;
+
+    public string Name { get; set; } = default!;
+
+    public Guid OUID { get; set; } = default;
+
+    public DateTimeOffset CreatedOn { get; set; } = default;
+
+    public bool Equals(Guid other) => Id.Equals(other);
+}
