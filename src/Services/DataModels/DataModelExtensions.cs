56f6df6c07bbae25e80249ab41eba80012e2cdba
Austin Vandersluis <austin@vandersluis.club>
2026-05-04 01:01:07 -0700

    Add: ModelResult extension for typed data extraction

---
+++ src/Services/DataModels/DataModelExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace OutsourceTracker.Services.DataModels;
+
+public static class DataModelExtensions
+{
+    public static TResult? ToResult<TResult>(this ModelResult result)
+    {
+        if (result.Success)
+        {
+            if (result.Data is TResult r)
+            {
+                return r;
+            }
+        }
+
+        return default;
+    }
+}
